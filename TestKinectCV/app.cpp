32eccc194f1b2574c902ec8f529a59a4ab15741c
chromedome <tinaylott@gmail.com>
2017-08-04 10:56:54 +0100

    First attempt at depth filtering

--- TestKinectCV/app.cpp
+++ TestKinectCV/app.cpp
@@ -125,33 +125,13 @@ void App::Tick(float deltaTime, osc::OutboundPacketStream &outBoundPS, UdpTransm
 		/*printf("MinVal: %5.0f ; MaxVal: %5.0f; MinX: %d MinY: %d; MaxX: %d MaxY %d   \n"
 			, vmin, vmax, idx_min[1], idx_min[0], idx_max[1], idx_max[0]);*/
 
-		cv::Mat NormMat;
-		cv::Mat FlippedMat;
-		cv::Mat ScalledMat;
-		cv::Mat BeforeColouredMat;
-		cv::Mat BeforeInvertedMat;
-		cv::Mat ThresholdedMat;
+		Mat BeforeColouredMat;
+		Mat BeforeInvertedMat;
 		Mat PlottedMat;
-		//cv::threshold(mat, ThresholdedMat, 255.0, 0 ,THRESH_TOZERO);
-		//cv::normalize(depthMatOriginal, NormMat, 0, 255, CV_MINMAX, CV_16UC1);
 		int colmap = 4;
 		cv::Mat DisplayMat;
 		depthMatOriginal.convertTo(BeforeColouredMat, CV_8UC3);
 		
-		// Reduce noise using inpaint
-		Mat _tmp, _tmp1, depthf; 
-				 //Mat(BeforeColouredMat2).convertTo(_tmp1, CV_64FC1);
-
-		Point minLoc; double minval, maxval;
-		minMaxLoc(BeforeColouredMat, &minval, &maxval, NULL, NULL);
-
-		Mat small_depthf;
-		resize(BeforeColouredMat, small_depthf, Size(), 0.2, 0.2);
-		//inpaint only the "unknown" pixels
-		inpaint(small_depthf, (small_depthf == 255), _tmp1, 5.0, 1);
-
-		resize(_tmp1, _tmp, BeforeColouredMat.size());
-		_tmp.copyTo(BeforeColouredMat, (BeforeColouredMat == 255));  //add the original signal back over the inpaint
 
 	
 		cv::applyColorMap(BeforeColouredMat, BeforeInvertedMat, colmap);
@@ -161,63 +141,29 @@ void App::Tick(float deltaTime, osc::OutboundPacketStream &outBoundPS, UdpTransm
 
 		// Second time arround with updating sand surface
 		
-		/*medianBlur(updatedSurface.clone(), Mat2Cropped, 5);
-		config->showImage(Mat2Cropped, config->cropRect);*/
-		
-		//printf("MinVal: %f ; MaxVal: %5.0f; MinX: %d MinY: %d; MaxX: %d MaxY %d   \n"
-		//, vmin, vmax, idx_min[1], idx_min[0], idx_max[1], idx_max[0]);
 
-		cv::Mat NormMat2;
-		cv::Mat FlippedMat2;
-		cv::Mat ScalledMat2;
-		cv::Mat BeforeColouredMat2;
-		cv::Mat BeforeInvertedMat2;
-		cv::Mat ThresholdedMat2;
+		Mat BeforeColouredMat2;
+		Mat BeforeInvertedMat2;
 		Mat PlottedMat2;
-		//cv::threshold(depthMatOriginal, ThresholdedMat2, 255.0, 0 ,THRESH_TOZERO);
+		
 		config->applyConfigurationSettingsToMatrix(Mat2Cropped);
-		//cv::normalize(Mat2Cropped, NormMat2, 0, 255, CV_MINMAX, CV_16UC1);
 
-
-
-		cv::Mat DisplayMat2;
+		Mat DisplayMat2;
 		
 		Mat2Cropped.convertTo(BeforeColouredMat2, CV_8UC3);
 		
-		Mat _tmp, _tmp1, depthf; //minimum observed value is ~440. so shift a bit
-		//Mat(BeforeColouredMat2).convertTo(_tmp1, CV_64FC1);
-
-		Point minLoc; double minval, maxval;
-		minMaxLoc(BeforeColouredMat2, &minval, &maxval, NULL, NULL);
-		//BeforeColouredMat2.convertTo(depthf, CV_8UC1, 255.0 / maxval);  //linear interpolation
-
-														   //use a smaller version of the image
-		Mat small_depthf; 
-		resize(BeforeColouredMat2, small_depthf, Size(), 0.2, 0.2);
-		//inpaint only the "unknown" pixels
-		inpaint(small_depthf, (small_depthf == 255), _tmp1, 5.0, 1);
-
-		resize(_tmp1, _tmp, BeforeColouredMat2.size());
-		_tmp.copyTo(BeforeColouredMat2, (BeforeColouredMat2 == 255));  //add the original signal back over the inpaint
 		
 		applyColorMap(BeforeColouredMat2, DisplayMat2, colmap);
 		//cv::bitwise_not(BeforeInvertedMat2, DisplayMat2);
-		
-		
-		
+			
 		// scale the values to map to the parameter they control
 		uint16 pitchVal = 48 + floor(double(35) / matCropped.cols * idx_max[1]);
 		uint16 lpfVal = 1000 + floor(double(2000) / matCropped.cols * idx_max[0]); 
 
-		// Send OSC
-		//printf("Columns: %d Rows: %d Pitch: %d CutoffFrequency: %d \n", matCropped.cols, matCropped.rows, pitchVal, lpfVal);
-		// Only send a message when the changes occur near the surface
-		// Exclude changes far above and below the surface
+		// Send OSC when an event is triggered
 		
-		//printf("Criteria 1 met.\n");
 		if (abs(vmax - currentMax) > 50 && vmax < 500) 
 		{
-			printf("Criteria 2 met should now send osc.\n");
 			try 
 			{
 				printf("Triggering OSC send. Pitch: %d Cuttoff %d ", pitchVal, lpfVal);
